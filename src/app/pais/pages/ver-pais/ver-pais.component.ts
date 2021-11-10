2848b08255b51ed8857004d823e4375d5bc9cb93
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-10 17:26:05 -0600

    Error corregido de componente html

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class VerPaisComponent {
-
-  termino: string = 'Hola mundo';
-
-  buscar() {
-    console.log(this.termino);
-  }
-}
+export class VerPaisComponent { }

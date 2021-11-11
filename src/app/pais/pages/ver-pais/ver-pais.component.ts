0a0fac76c249f4c6f73d2592a4250a745c28b721
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-11 10:48:47 -0600

    PorPaisComponent extendido

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais-service';
 
@@ -12,6 +12,8 @@ import { PaisService } from '../../services/pais-service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
@@ -19,13 +21,13 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {
 
-
     /*CON OPERADOR SWITCHMAP*/
     this.activatedRoute.params
       .pipe(
-        switchMap(({ id }) => this.paisService.getPaisPorId(id))
+        switchMap(({ id }) => this.paisService.getPaisPorId(id)),
+        tap(console.log)
       )
-      .subscribe(resp => console.log)
+      .subscribe(pais => this.pais = pais[0])
 
     /*FORMA DE TRABAJAR SIN EL OPERADOR SWITCHMAP*/
     // this.activatedRoute.params

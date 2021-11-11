918810298551dc1ff021627ef8a34ddbbdc43300
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-10 18:57:51 -0600

    Mostrando paises en por-pais.component

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais-service';
 
 @Component({
@@ -10,14 +11,19 @@ import { PaisService } from '../../services/pais-service';
 export class PorPaisComponent {
   termino: string = 'Hola mundo';
   hayError: boolean = false;
+  paises: Country[] = [];
+
   constructor(private paisService: PaisService) { }
 
   buscar() {
     this.hayError = false;
     this.paisService.buscarPais(this.termino)
       .subscribe({
-        next: (paises) => console.log,
-        error: (e) => this.hayError = true
+        next: (paises) => { console.log(paises); this.paises = paises },
+        error: (e) => {
+          this.hayError = true;
+          this.paises = [];
+        }
       })
   }
 }

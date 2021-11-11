0d0b269c8ef07187277bc3d085f5750089d532de
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-11 14:25:24 -0600

    Agregando apartado de sugerencias a input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,16 +6,25 @@ import { PaisService } from '../../services/pais-service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li{
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
+
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
@@ -30,6 +39,10 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
-
+    this.mostrarSugerencias = true;
+    this.termino = termino;
+    this.paisService.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos = paises.splice(0, 3))
   }
+
 }

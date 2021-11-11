1c0c0a09e34ee32dcd535893f8c03b57804ee880
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-11 13:07:47 -0600

    get httpParams y formateo
    Uso de httpParams para reducir el consumo de la api. Formateo de verPaisComponent html

--- src/app/pais/services/pais-service.ts
+++ src/app/pais/services/pais-service.ts
@@ -7,25 +7,28 @@ import { Country } from '../interfaces/pais.interface';
   providedIn: 'root'
 })
 export class PaisService {
+
   private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  get httpParams() {
+    return new HttpParams().set('fields', 'name,capital,population,cca2,flag,flags')
+  };
 
   constructor(private http: HttpClient) { }
 
   buscarPais(pais: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${pais}`;
-    return this.http.get<Country[]>(url);
-    // const params = new HttpParams().set('name', termino);
-    // return this.http.get(this.apiUrl, { params });
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   buscarCapital(capital: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${capital}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   buscarRegion(region: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${region}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
 
   getPaisPorId(pais: string): Observable<Country[]> {
